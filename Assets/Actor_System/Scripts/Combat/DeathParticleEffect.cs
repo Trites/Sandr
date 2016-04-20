051402a913ba1991a57b9664ae44a5a3b25dc150
Trites <gustaf.svanerud@gmail.com>
2016-04-20 09:37:16 +0200

    Extracted death particle effect to its own prefab.

---
+++ Assets/Actor_System/Scripts/Combat/DeathParticleEffect.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathParticleEffect : MonoBehaviour {
+
+	ParticleEmitter emitter;
+	
+	public void Awake(){
+		
+		
+	}
+
+	public void Spawn(Vector2 impactPosition, Vector2 impactVelocity, Vector2 particleVelocity, float impactMass, float particleMassSpan, float particleMassAdjust, int count){
+		
+		emitter = GetComponent<ParticleEmitter>();
+		emitter.Emit(count);
+		
+		//emitter.emit = false;
+		
+		Particle[] particles = emitter.particles;
+		
+		print("Simulating " + particles.Length + " particles.");
+		for(int i = 0; i < particles.Length; i++){
+			
+			float particleMass = Random.value * particleMassSpan + particleMassAdjust;
+			particles[i].velocity = Util.getBounceVelocity(particles[i].position, impactPosition, impactVelocity,  particleVelocity, particleMass, impactMass);
+		}
+		
+		emitter.particles = particles;
+	}
+}
--- Assets/Actor_System/Scripts/Combat/DieOnHit.cs
+++ Assets/Actor_System/Scripts/Combat/DieOnHit.cs
@@ -1,40 +1,25 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class DieOnHit : MonoBehaviour {
-
-	ParticleEmitter emitter;
+	
+	public GameObject particleEffect;
 	
 	void Start(){
 		
-		emitter = GetComponent<ParticleEmitter>();
-		emitter.emit = false;
-		
-		if(emitter == null)
+		if(particleEffect == null)
 			Debug.LogError("No emitter!");
-	}
-	
-	void Update () {
-	
 	}
 	
 	public void HitBy(MeleeWeapon.WeaponHitData hitData){
 		
 		print("Hit!");
-		emitter.Emit(50);
 		
-		Particle[] particles = emitter.particles;
+		DeathParticleEffect emitter = (Instantiate(particleEffect, transform.position, Quaternion.identity) as GameObject).GetComponent<DeathParticleEffect>();
 		
-		print("Simulating " + particles.Length + " particles.");
-		for(int i = 0; i < particles.Length; i++){
+		emitter.Spawn(hitData.HitPosition, hitData.Direction*10f, Vector2.zero, hitData.Force, 25f, 5, 50);
 			
-			float particleMass = Random.value * 10f + 2f;
-			//particles[i].position = hitData.HitPosition;
-			particles[i].velocity = Util.getBounceVelocity(particles[i].position, hitData.HitPosition, hitData.Direction*20,  -Vector2.right*10, particleMass, hitData.Force);
-		}
-		
-		emitter.particles = particles;
-		
 		Debug.DrawLine(transform.position, hitData.HitPosition, Color.red, 3f);
+		Destroy(gameObject);
+		
 	}
 }
